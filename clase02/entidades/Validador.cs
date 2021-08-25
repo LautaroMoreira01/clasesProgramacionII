b2f73b440b3d1f516edcf83c9ddd313541e10155
Lautaro <lauty2001moreira@gmail.com>
2021-08-24 21:28:28 -0300

    .

---
+++ clase02/entidades/Validador.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Entidades
+{
+    public class Validador
+    
+    {
+        public static bool Validar(int valor, int min, int max)
+        {
+            bool rta = false; 
+            if  (valor >= min && valor <= max)
+            {
+                rta = true;
+            }
+            return rta;
+        }
+    }
+
+}
